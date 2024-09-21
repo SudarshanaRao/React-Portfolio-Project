7c99a5bacb936541f29419a57c864527d16dac9b
SudarshanaRao <pandu.sudha2003@gmail.com>
2024-09-21 22:42:45 +0530

    updated Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import headerBg from '../images/header-bg-color.png';  
 import menuIcon from '../images/menu-black.png';
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
       {/* Background image div */}
@@ -12,13 +27,13 @@ function Navbar() {
       </div>
 
       {/* Navbar */}
-      <nav className="px-5 lg:px-8 py-4 xl:px-[8%] w-full fixed z-50">
+      <nav className={`px-5 lg:px-8 py-4 xl:px-[8%] w-full fixed z-50 transition-all duration-300 ${isScrolled ? 'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm' : ''}`}>
         <div className="container mx-auto flex justify-between items-center">
           <Link to="/" className="text-xl font-bold">Sudarshana Rao</Link>
           
           {/* Menu list */}
-          <div id="listItems" className="hidden md:flex gap-4 lg:gap-6 rounded-full py-2 px-8 shadow-sm bg-opacity-50 font-Ovo cursor-pointer">
-          <Link to="home" smooth={true} duration={500} className="">Home</Link>
+          <div id="listItems" className={`hidden md:flex gap-4 lg:gap-6 rounded-full py-2 px-8 shadow-sm cursor-pointer transition-all duration-300 ${isScrolled ? 'bg-white shadow-sm bg-opacity-50' : ''}`}>
+            <Link to="home" smooth={true} duration={500} className="">Home</Link>
             <Link to="about" smooth={true} duration={500} className="">About Me</Link>
             <Link to="projects" smooth={true} duration={500} className="">Projects</Link>
             <Link to="contact" smooth={true} duration={500} className="">Contact Me</Link>
